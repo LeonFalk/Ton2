c9c3f29b17b52912ab9ca6511c10c1f2fdad9602
LeonFalk <93582320+LeonFalk@users.noreply.github.com>
2021-11-24 16:06:25 +0100

    Create Main.py

---
+++ Praxisproblem1/Main.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 24 14:58:02 2021
+
+@author: Leon
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.io import wavfile
+samplerate, data = wavfile.read('Datei_1.wav')
+y = 0
+
+#Ausgabe der Array-Länge, Samplerate und dem Array
+print(data.size, samplerate, data)
+
+array_integr = np.zeros((data.size))
+
+for i in range(-1, (data.size)*-1, -1):
+     y = y + data[i]**2                    #um Positive Teile zu nutzen?
+     
+     array_integr[i] = y
+
+#rausrechnen der Samplerate -> Normierung...     ??
+array_integr = array_integr/samplerate
+
+#Anzeige in Sekunden fehlt noch 
+plt.plot(array_integr)
+plt.show()
